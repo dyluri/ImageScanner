9b7fdaae47fec84b39f9e2755b12ee309b30c2ec
dyluri <dyluri007@gmail.com>
2023-04-22 16:38:58 -0700

    implemented basic camera stream with comments

--- entry.sh
+++ entry.sh
@@ -3,6 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 $DIR/bootstrap.sh $DIR $DIR/venv
 
-$DIR/venv/bin/python $DIR/src/main.py $@
+$DIR/venv/bin/python $DIR/src/main.py $@ --port 50051
+#There are other cameras also avalible
+#50052 = OAK 1
+#50053 = OAK 2
+#50054 = OAK 4
 
 exit 0
--- src/main.py
+++ src/main.py
@@ -53,13 +53,16 @@ from kivy.properties import StringProperty  # noqa: E402
 
 class ImageScannerApp(App):
     """Base class for the main Kivy app."""
-
-    def __init__(self) -> None:
+    def __init__(self, address: str, port: int, stream_every_n: int) -> None:
         super().__init__()
+        self.address = address
+        self.port = port
+        self.stream_every_n = stream_every_n
+        self.image_decoder = TurboJPEG()
 
         self.counter: int = 0
 
-        self.async_tasks: List[asyncio.Task] = []
+        self.tasks: List[asyncio.Task] = []
 
     def build(self):
         return Builder.load_file("res/main.kv")
@@ -73,20 +76,82 @@ class ImageScannerApp(App):
             # we don't actually need to set asyncio as the lib because it is
             # the default, but it doesn't hurt to be explicit
             await self.async_run(async_lib="asyncio")
-            for task in self.async_tasks:
+            for task in self.tasks:
                 task.cancel()
+                
+            #Configuring the camera client
+            
+        config = ClientConfig(address = self.address, port = self.port)
+        client = OakCameraClient(config)
 
+            #stream the camera's frames
+        self.tasks.append(asyncio.ensure_future(self.stream_camera(client)))
         # Placeholder task
-        self.async_tasks.append(asyncio.ensure_future(self.template_function()))
 
-        return await asyncio.gather(run_wrapper(), *self.async_tasks)
-
-    async def template_function(self) -> None:
-        """Placeholder forever loop."""
+        return await asyncio.gather(run_wrapper(), *self.tasks)
+    async def stream_camera(self, client: OakCameraClient):
+        """This task listens to one camera stream and puts all 4 streams together for the kv file"""
         while self.root is None:
             await asyncio.sleep(0.01)
-
+        response_stream = None
+        
         while True:
+            state = await client.get_state()
+            if state.value not in [
+                service_pb2.ServiceState.IDLE,
+                service_pb2.ServiceState.RUNNING,
+            ]:
+                #Cancel the stream if it exists
+                if response_stream is not None:
+                    response_stream.cancel()
+                    response_stream = None 
+                print("Camera 1 is not streaming or ready to stream")
+                await asyncio.sleep(0.1)
+                continue
+            
+            if response_stream == None:
+                response_stream = client.stream_frames(every_n= self.stream_every_n)
+            try:
+                #Will not run until there is a value in response_stream
+                response: oak_pb2.StreamFramesReply = await response_stream.read()
+                #Checks that response is not false or None, and that the stream hasnt ended
+                assert response and response != grpc.aio.EOF, "End of Stream" 
+                
+            except Exception as e:
+                print("Error: ", e)
+                response_stream.cancel()
+                response_stream = None
+                #loop starts over 
+                continue
+            
+            #Get the sync frame (frame that the rest of the stream is based off)
+            #Colon just means what data it is expecting to recieve
+            frame: oak_pb2.OakSyncFrame = response.frame
+            
+            #Get image and show
+            for view_name in ['rgb', 'disparity', 'left', 'right']:
+                try: 
+                    #decode image
+                    img = self.image_decoder.decode(
+                        getattr(frame,view_name).image_data
+                    )
+                    texture = Texture.create(
+                        #Creates the texture the height and width of img
+                        size = (img.shape[1], img.shape[2]), icolorfmt = 'bgr'
+                    )
+                    texture.flip_verticle()
+                    #Puts the image onto the texture variable, stored in blue green red format. 
+                    texture.blit_buffer(
+                        img.tobytes(),
+                        colorfmt = 'bgr', 
+                        bufferfmt = 'ubyte',
+                        mipmap_generation = False
+                    )
+                    #Puts the texture in the proper tab for the GUI
+                    self.root.ids[view_name].texture = texture
+                except Exception as e:
+                    print("Error", e)
+                    
             await asyncio.sleep(1.0)
 
             # increment the counter using internal libs and update the gui
@@ -100,12 +165,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Image-Scanner")
 
     # Add additional command line arguments here
-
+    parser.add_argument('--port', type= int, required= True, help = 'The Camera Port')
+    parser.add_argument('--address', type= str, default = 'localhost', help = "The camera address")
+    parser.add_argument('--stream-every-n', type=int, default = 2, help= 'Cam stream frequency')
+    
+    
     args = parser.parse_args()
 
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(ImageScannerApp().app_func())
+        loop.run_until_complete(
+            ImageScannerApp(args.address, args.port, args.stream_every_n).app_func()
+            )
     except asyncio.CancelledError:
         pass
     loop.close()
